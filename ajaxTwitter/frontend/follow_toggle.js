5fd9341f6caf3e1a9d5940863359a55ba54e1fcd
duskstone <jedgardromero@gmail.com>
2020-05-14 16:28:54 -0400

    Changing FollowState appropiately,getting rid of .text and making render a protoype method

--- ajaxTwitter/frontend/follow_toggle.js
+++ ajaxTwitter/frontend/follow_toggle.js
@@ -1,13 +1,21 @@
 class FollowToggle {
     // what is el in this case? a user?
     constructor(el) {
-        debugger;
         this.$el = $(el);
         this.userId = this.$el.data("user-id");
         this.followState = this.$el.data("initial-follow-state");
+        this.render();
         
-
     }
-}
+};
+
+FollowToggle.prototype.render = function() { //text here refers to button or do we need add a text key?
+    //we just have to reassign this.followState
+  if (this.followState === "unfollowed") {
+    this.followState = "followed";
+  } else {
+    this.followState = "unfollowed";
+  };
+};
 
 module.exports = FollowToggle
